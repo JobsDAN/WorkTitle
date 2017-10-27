f73a2fada5793a835488f5beff3eb2ea8266fe02
Tokarev Maxim <nepfion96@gmail.com>
2017-10-27 17:03:31 +0300

    Added simple camera
    Camera movement when cursor reach the edge of screen and with arrow keys.
    
    Button 'F' -- focuses on player

---
+++ Assets/Source/CameraMovement.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    [SerializeField]
+    private Transform target;
+
+    private float currentZoom;
+    private float offsetZ;
+
+    [SerializeField]
+    private int MIN_ZOOM;
+    [SerializeField]
+    private int MAX_ZOOM;
+
+    private const int BOUNDARY_SIZE = 30;
+
+    [SerializeField]
+    private int cameraSpeed;
+    
+    private bool isFocused = false;
+    private Vector3 cameraMovement;
+
+	void Start () {
+        cameraMovement = Vector3.zero;
+        offsetZ = Mathf.Sin(Mathf.Deg2Rad * (90 - transform.rotation.eulerAngles.x)) * transform.position.y
+                  / Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.x);
+    }
+	
+	void Update () {
+        CameraManagementInput();
+        Zoom();
+        Move();
+	}
+
+    private void CameraManagementInput()
+    {
+        if (Input.GetKeyDown(KeyCode.F)) // Focus | follow
+        {
+            isFocused = !isFocused;
+        }
+        
+        // ...
+
+
+        if (isFocused)
+        {
+            return;
+        }
+        
+
+        if (Input.mousePosition.x > Screen.width - BOUNDARY_SIZE || Input.GetKey(KeyCode.RightArrow))
+        {
+            cameraMovement += Vector3.right;
+        }
+        if (Input.mousePosition.x < BOUNDARY_SIZE || Input.GetKey(KeyCode.LeftArrow))
+        {
+            cameraMovement += Vector3.left;
+        }
+        if (Input.mousePosition.y > Screen.height - BOUNDARY_SIZE || Input.GetKey(KeyCode.UpArrow))
+        {
+            cameraMovement += Vector3.forward;
+        }
+        if (Input.mousePosition.y < BOUNDARY_SIZE || Input.GetKey(KeyCode.DownArrow))
+        {
+            cameraMovement += Vector3.back;
+        }
+
+        cameraMovement.Normalize();
+    }
+
+    private void Zoom()
+    {
+        float scrollDelta = Input.mouseScrollDelta.y;
+        if (scrollDelta != 0)
+        {
+            currentZoom += scrollDelta;
+
+            if (MIN_ZOOM > currentZoom || currentZoom > MAX_ZOOM)
+            {
+                currentZoom = Mathf.Clamp(currentZoom, MIN_ZOOM, MAX_ZOOM);
+                return;
+            }
+
+            transform.Translate(0, 0, scrollDelta);
+            offsetZ = Mathf.Sin(Mathf.Deg2Rad * (90 - transform.rotation.eulerAngles.x)) * transform.position.y
+                      / Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.x);
+        }
+    }
+
+    private void Move()
+    {
+        if (isFocused)
+        {
+            transform.position = new Vector3(target.position.x, transform.position.y, target.position.z - offsetZ);
+        }
+        else if (cameraMovement != Vector3.zero)
+        {
+            transform.position += cameraMovement * cameraSpeed;
+            cameraMovement = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Source/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float obstacleRadius;
+
+    private Vector3 targetPosition;
+    private Quaternion rotationToTarget;
+
+    private Camera camera;
+    private CharacterController characterController;
+
+	void Start () {
+        targetPosition = transform.position;
+
+        characterController = GetComponent<CharacterController>();
+        camera = Camera.main;
+	}
+	
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            targetPosition = GetTargetPosition();
+        }
+
+        if (Vector3.Distance(transform.position, targetPosition) > obstacleRadius)
+        {
+            Move();
+        }
+	}
+
+    private void Move() {
+        transform.rotation = GetRotationToTarget();
+
+        characterController.Move(transform.forward * movementSpeed * Time.deltaTime);
+    }
+
+    private Vector3 GetTargetPosition()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            return hit.point;
+        }
+
+        return transform.position;
+    }
+
+    private Quaternion GetRotationToTarget()
+    {
+        Quaternion newRotation = Quaternion.LookRotation(targetPosition - transform.position);
+        newRotation.x = 0;
+        newRotation.z = 0;
+
+        newRotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
+
+        return newRotation;
+    }
+}
